75b64df88023c3953766f1cb912117e6165a7bb5
Jenifer29b <Jenifer29b@gmail.com>
2026-02-13 13:29:50 +0530

    mongodb connection

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,9 +4,10 @@ dotenv.config()
 
 
 const connectDB = async () => {
+    
     try {
        const connection = await mongoose.connect(process.env.MONG0DBCONNECTIONSTRING)
-      console.log('MongoDB connected')
+        console.log('MongoDB connected')
       return connection;
     } catch (error) {
         console.log('MongoDB connection error', error)
